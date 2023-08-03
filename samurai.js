65ddeeaae6afa0f0edbbfdb75350604f4d1be1ef
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-03 22:41:38 +0200

    create samurai following doc-instructions & no errors but samurai not yet visible

---
+++ samurai.js
@@ -0,0 +1,38 @@
+import * as THREE from "three";
+import WebGL from "three/addons/capabilities/WebGL.js";
+
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const loader = new GLTFLoader();
+
+loader.load(
+  "/scene.gltf",
+  function (gltf) {
+    scene.add(gltf.scene);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
