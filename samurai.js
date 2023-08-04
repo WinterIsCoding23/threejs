6a416b87704c8076c435735c407047291f7e5d62
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-04 11:00:16 +0200

    uncomment black bg-color & restructure public & add AmbientLight and reduce y-position and scale of samurai

--- samurai.js
+++ samurai.js
@@ -4,12 +4,16 @@ import WebGL from "three/addons/capabilities/WebGL.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xdddddd);
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
+
+const hlight = new THREE.AmbientLight(0x404040, 100);
+scene.add(hlight);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -18,11 +22,11 @@ document.body.appendChild(renderer.domElement);
 const loader = new GLTFLoader();
 
 loader.load(
-  "/scene.gltf",
+  "/samurai/scene.gltf",
   function (gltf) {
     gltf.scene.rotation.y = Math.PI / 8;
-    gltf.scene.position.y = 3;
-    gltf.scene.scale.set(100, 100, 100);
+    gltf.scene.position.y = 0.5;
+    gltf.scene.scale.set(1, 1, 1);
     scene.add(gltf.scene);
   },
   undefined,
