dc41a7340e7d2400830951799311919621d16b65
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-04 17:19:55 +0200

    create sphere.jsx & increase camera.position.z

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Cube from "./components/cube";
 import Line from "./components/line";
+import Sphere from "./components/sphere";
 import Samurai from "./components/samurai";
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
     <div>
       <Cube />
       <Line />
+      <Sphere />
       <Samurai />
     </div>
   );
---
+++ src/components/sphere.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef } from "react";
+import * as THREE from "three";
+import WebGL from "three/addons/capabilities/WebGL.js";
+
+const Sphere = () => {
+  const sphereRef = useRef();
+
+  useEffect(() => {
+    if (WebGL.isWebGLAvailable()) {
+      const scene = new THREE.Scene();
+      scene.background = new THREE.Color("#3ab740");
+      const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+
+      const renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      // document
+      //   .getElementById("myThreeJsCanvas")
+      //   .appendChild(renderer.domElement);
+      sphereRef.current.appendChild(renderer.domElement);
+
+      const geometry = new THREE.SphereGeometry(15, 32, 16);
+      const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+      const sphere = new THREE.Mesh(geometry, material);
+
+      scene.add(sphere);
+
+      camera.position.z = 50;
+
+      function animate() {
+        requestAnimationFrame(animate);
+        sphere.rotation.x += 0.01;
+        sphere.rotation.y += 0.01;
+        renderer.render(scene, camera);
+      }
+      animate();
+    } else {
+      const warning = WebGL.getWebGLErrorMessage();
+      // document.getElementById("myThreeJsCanvas").appendChild(warning);
+      sphereRef.current.appendChild(warning);
+    }
+  }, []);
+
+  return <div ref={sphereRef} />;
+};
+
+export default Sphere;
