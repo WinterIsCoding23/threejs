cee35c774c94d126e3150d0317f7847454ce995f
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-04 16:50:16 +0200

    change line.js to line.jsx & add useEffect and useRef & render it in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Cube from "./components/cube";
+import Line from "./components/line";
 import Samurai from "./components/samurai";
 
 export default function App() {
   return (
     <div>
       <Cube />
+      <Line />
       <Samurai />
     </div>
   );
